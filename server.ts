0c9d414a8c80b09092604e4a5f445babb6a89625
Makhayev <y.makhayev@alabs.team>
2023-01-26 09:11:04 +0600

    feat(videochat): add videochat features for backend

--- server.ts
+++ server.ts
@@ -50,22 +50,14 @@ const io = require("socket.io")(server, {
   },
 });
 io.on("connection", (socket) => {
-  socket.emit("me", socket.id);
-  console.log(socket.id);
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callEnded");
-  });
+  socket.on("join-room", (roomId, userId) => {
+    console.log(roomId, userId);
+    socket.join(roomId);
+    socket.to(roomId)?.emit("user-connected", userId);
 
-  socket.on("callUser", (data) => {
-    io.to(data.userToCall).emit("callUser", {
-      signal: data.signalData,
-      from: data.from,
-      name: data.name,
+    socket.on("disconnect", () => {
+      socket.to(roomId)?.emit("user-disconnected", userId);
     });
-  });
-
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal);
   });
 });
 

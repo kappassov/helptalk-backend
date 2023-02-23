a2e305510cfd9e95d1545d63d0eff8eac303f3bf
Makhayev <y.makhayev@alabs.team>
2023-02-23 22:57:14 +0600

    fix(avatars): added upload avatar route

--- app/routes/user.ts
+++ app/routes/user.ts
@@ -1,9 +1,11 @@
 export {};
 const express = require("express");
-const UserController  = require("../controllers/user.controller");
+const UserController = require("../controllers/user.controller");
 const authMiddleware = require("../middlewares/auth-middleware");
 const adminRouter = express.Router();
 
 adminRouter.get("/user/getByEmail", authMiddleware, UserController.getByEmail);
+
+adminRouter.post("/user/uploadAvatar", UserController.upload_avatar);
 
 module.exports = adminRouter;
